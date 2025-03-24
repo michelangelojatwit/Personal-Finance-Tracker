fa485aa21f70aaadc110069c3dbe8b192fb0fe01
Joemi <Joemi@10.220.81.144>
2025-03-24 00:23:53 -0400

    Added notation to the Module-info.java

--- Personal Finance tracker/src/module-info.java
+++ Personal Finance tracker/src/module-info.java
@@ -5,8 +5,9 @@
  * 
  */
 module Classwork {
+	//Make sure you do not touch this, otherwise the GUI will not load - Joe M 
     requires javafx.controls;
     requires javafx.fxml;
 
-    exports Classwork;  // This allows JavaFX to access your GUI class
+    exports Classwork;
 }
